c9b8b4ac7142d9c8c7fe2dd0871c4c1cc07776b6
Misael Bandeira Silveira <misael.bandeira@gmail.com>
2023-06-10 17:57:43 -0300

    fix: fixed method in api

--- src/pages/api/users/[username]/schedule.api.ts
+++ src/pages/api/users/[username]/schedule.api.ts
@@ -62,7 +62,7 @@ export default async function handler(
       name,
       email,
       observations,
-      date: schedulingDate.tz('America/Sao_Paulo').toDate(),
+      date: schedulingDate.toDate(),
       user_id: user.id,
     },
   })
